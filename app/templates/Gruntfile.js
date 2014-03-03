e681140658feea34befca05e067b3f14fb2b4099
Clive Walkden <clivewalkden@gmail.com>
2014-03-03 20:51:57 +0000

    Updated after running test

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -11,17 +11,17 @@ module.exports = function (grunt) {
     // Metadata.
     pkg: grunt.file.readJSON('package.json'),
     banner: '/*!\n' +
-      '* <%= pkg.name %> - for jQuery 1.7+\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* <%%= pkg.name %> - for jQuery 1.7+\n' +
+      '<%%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
       '*\n' +
-      '* Copyright <%= grunt.template.today("yyyy") %>, <%= pkg.author.name %> (http://clivewalkden.co.uk)\n' +
+      '* Copyright <%%= grunt.template.today("yyyy") %>, <%%= pkg.author.name %> (http://clivewalkden.co.uk)\n' +
       '*\n' +
-      '* @package <%= pkg.description %>\n' +
-      '* @author <%= pkg.author.name %> (<%= pkg.author.url %>)\n' +
-      '* @version <%= pkg.version %>\n' +
-      '* @license <%= props.license %>\n' +
-      '* @copyright Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %> (<%= pkg.author.url %>)\n' +
-      '* @date: <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '* @package <%%= pkg.description %>\n' +
+      '* @author <%%= pkg.author.name %> (<%%= pkg.author.url %>)\n' +
+      '* @version <%%= pkg.version %>\n' +
+      '* @license <%%= props.license %>\n' +
+      '* @copyright Copyright (c) <%%= grunt.template.today("yyyy") %> <%%= pkg.author.name %> (<%%= pkg.author.url %>)\n' +
+      '* @date: <%%= grunt.template.today("yyyy-mm-dd") %>\n' +
       '*/\n\n',
     // Task configuration.
     clean: {
@@ -50,7 +50,7 @@ module.exports = function (grunt) {
       all: {
         options: {
           urls: ['1.7.0','1.7.1','1.7.2','1.8.0','1.8.1','1.8.2','1.8.3','1.9.0','1.9.1','1.10.0','1.10.1','1.10.2','1.11.0','2.0.0','2.0.1','2.0.2','2.0.3','2.1.0'].map(function(version) {
-            return 'http://localhost:<%= connect.server.options.port %>/test/<%%= pkg.name %>.html?jquery=' + version;
+            return 'http://localhost:<%%= connect.server.options.port %>/test/<%%= pkg.name %>.html?jquery=' + version;
           })
         }
       }
